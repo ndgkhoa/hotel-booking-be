b04586e4b694885341d63048e186c0d010804906
khoa <user.iamjasper@gmail.com>
2024-06-22 07:26:46 +0700

    v1: done

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import connectDB from './config/db'
 import authRoutes from './routes/auth'
 import usersRoutes from './routes/users'
 import hotelsRoutes from './routes/hotels'
+import bookingsRoutes from './routes/bookings'
 import cookieParser from 'cookie-parser'
 import cloudinaryConfig from './config/cloudinaryConfig'
 
@@ -27,6 +28,7 @@ app.use(
 app.use('/api/auth', authRoutes)
 app.use('/api/hotels', hotelsRoutes)
 app.use('/api/users', usersRoutes)
+app.use('/api/bookings', bookingsRoutes)
 // app.get('*', (req: Request, res: Response) => {
 //     res.sendFile(path.join(__dirname, '../../frontend/dist/index.html'))
 // })
--- src/routes/hotels.ts
+++ src/routes/hotels.ts
@@ -1,9 +1,10 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
 import upload from '../config/multerConfig'
 import HotelsController from '../controllers/HotelsController'
 import verifyToken from '../middlewares/auth'
 import hotelValidation from '../middlewares/hotelValidation'
 import { param } from 'express-validator'
+import Hotel from '../models/hotel'
 
 const router = express.Router()
 
@@ -14,7 +15,15 @@ router.post(
     upload.array('imageFiles', 6),
     HotelsController.createHotel,
 )
-
+router.get('/', async (req: Request, res: Response) => {
+    try {
+        const hotels = await Hotel.find().sort('-lastUpdated')
+        res.json(hotels)
+    } catch (error) {
+        console.log('error', error)
+        res.status(500).json({ message: 'Error fetching hotels' })
+    }
+})
 router.get('/search', HotelsController.search)
 router.get('/', verifyToken, HotelsController.getAllHotels)
 router.get(
@@ -28,5 +37,11 @@ router.put(
     upload.array('imageFiles'),
     HotelsController.updateHotel,
 )
+router.post(
+    '/:hotelId/bookings/payment-intent',
+    verifyToken,
+    HotelsController.payment,
+)
+router.post('/:hotelId/bookings', verifyToken, HotelsController.booking)
 
 export default router
